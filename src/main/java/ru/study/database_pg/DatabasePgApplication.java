497f5dd913784e831ae2da2e7372a57f83211edb
Maksim <nersus1337@mail.ru>
2025-05-02 13:12:17 +0700

    Init the project

---
+++ src/main/java/ru/study/database_pg/DatabasePgApplication.java
@@ -0,0 +1,13 @@
+package ru.study.database_pg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasePgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/study/database_pg/DatabasePgApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.database_pg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabasePgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
